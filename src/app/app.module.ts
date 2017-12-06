2b918a125f7faecda83724edb3b9b81facbbf01e
Piyush Mishra <you@example.com>
2017-12-06 19:11:05 +0530

    no horizontal bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { LoadtableComponent } from './loadtable/loadtable.component';
 import { CardComponent } from './card/card.component';
 import { CardsComponent } from './cards/cards.component';
-import { WhatWeDocardComponent } from './what-we-docard/what-we-docard.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     CardsComponent,
-    WhatWeDocardComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent, CardComponent, CardsComponent,WhatWeDocardComponent]
+  bootstrap: [AppComponent, CardComponent, CardsComponent, NavBarComponent]
 })
 export class AppModule { }
+
+// const appRoutes: Routes = [
+//   { path: 'crisis-center', component: CrisisListComponent },
+//   { path: 'hero/:id',      component: HeroDetailComponent },
+//   {
+//     path: 'heroes',
+//     component: HeroListComponent,
+//     data: { title: 'Heroes List' }
+//   },
+//   { path: '',
+//     redirectTo: '/heroes',
+//     pathMatch: 'full'
+//   },
+//   { path: '**', component: PageNotFoundComponent }
+// ];
--- src/app/nav-bar/nav-bar.component.spec.ts
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WhatWeDocardComponent } from './what-we-docard.component';
+import { NavBarComponent } from './nav-bar.component';
 
-describe('WhatWeDocardComponent', () => {
-  let component: WhatWeDocardComponent;
-  let fixture: ComponentFixture<WhatWeDocardComponent>;
+describe('NavBarComponent', () => {
+  let component: NavBarComponent;
+  let fixture: ComponentFixture<NavBarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WhatWeDocardComponent ]
+      declarations: [ NavBarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(WhatWeDocardComponent);
+    fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
